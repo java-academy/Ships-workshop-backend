c47f4f5b250ee3ee9c08e9351dcda9b583b59f79
Kamil Jankowski <1990.kamil.jankowski@gmail.com>
2020-12-15 09:27:29 +0100

    Review done: Jacoco plugin configured. Separate method for heroku check created

--- src/main/java/com/ships/ShipsRoomServiceApplication.java
+++ src/main/java/com/ships/ShipsRoomServiceApplication.java
@@ -11,11 +11,15 @@ import org.tinylog.Logger;
 public class ShipsRoomServiceApplication {
 	public static void main(String[] args) {
 		Logger.debug("Application start");
+		configureHeroku();
+		SpringApplication.run(ShipsRoomServiceApplication.class, args);
+	}
+
+	private static void configureHeroku() {
 		String ENV_PORT = System.getenv().get("PORT");
 		String ENV_DYNO = System.getenv().get("DYNO");
 		if(ENV_PORT != null && ENV_DYNO != null) {
 			System.getProperties().put("server.port", ENV_PORT);
 		}
-		SpringApplication.run(ShipsRoomServiceApplication.class, args);
 	}
 }
