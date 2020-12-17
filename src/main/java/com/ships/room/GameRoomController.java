177ea08fcdab8b5645a7d461d2c17a43749cc73d
MJ211 <jedrzejkiewicz.michal@gmail.com>
2020-12-17 14:20:52 +0100

    Removed TODO comments

--- src/main/java/com/ships/room/GameRoomController.java
+++ src/main/java/com/ships/room/GameRoomController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//TODO: create .properties file which will bypass checking coverage for the equals method etc. (lombok)
-
 @RestController
 @RequestMapping(value = "/room", produces = "application/json; charset=UTF-8")
 @AllArgsConstructor
